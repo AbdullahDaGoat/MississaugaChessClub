7882dbfa8ef1973d6a3d87fc9e3870c6d323edec
Faheem K <faheemk.dev@gmail.com>
2023-08-22 16:48:21 -0400

    updated email form on contact page with new submit script

--- src/user/contactEmail.js
+++ src/user/contactEmail.js
@@ -1,26 +1,34 @@
+/* 
+    Sends email when contact form is submitted
+*/
+
+// form inputs:
 const username = document.getElementById("name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const message = document.getElementById("text");
 
+// form:
 const submit = document.getElementsByClassName("form-contact")[0];
 
+//triggers when form submitted
 submit.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("email form triggered");
 
-    //email body:
+    //creating the email body:
     let emailBody = `
-    "Name: " +
-    ${username.value} +
-    "<br> Email: " +
-    ${email.value} +
-    "<br> Phone:" +
-    ${phone.value} +
-    "<br> Message: " +
+    Name:  
+    ${username.value} 
+    <br> Email: 
+    ${email.value} 
+    <br> Phone: 
+    ${phone.value} 
+    <br> Message:  
     ${message.value}
     `;
 
+    //sends email:
     Email.send({
         SecureToken: "3a9c6519-6fbc-46bd-919e-7e0ad0bbe1ed",
         To: "mississaugachessclub@gmail.com",
@@ -33,4 +41,4 @@ submit.addEventListener("submit", (e) => {
             console.log(response);
         })
 
-})
+});
